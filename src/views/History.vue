5f745a1c8adca0741b90761fd40d116990efd69d
KotaHv <92137267+KotaHv@users.noreply.github.com>
2023-05-17 19:45:20 +0800

    feat: reset btn

--- src/views/History.vue
+++ src/views/History.vue
@@ -2,12 +2,17 @@
     <v-container>
         <v-card>
             <v-card-title class="font-weight-bold">
-                <span> IP History</span>
+                <v-row justify="space-between">
+                    <v-col cols="auto">IP History</v-col>
+                    <v-col cols="auto">
+                        <v-btn @click="reset" variant="text" elevation="0" icon="mdi-sort-variant-remove"></v-btn>
+                    </v-col>
+                </v-row>
             </v-card-title>
             <v-divider />
             <v-card-text class="table-container">
                 <v-data-table-server :headers="headers" :items="histories" :loading="loading" :items-length="total"
-                    item-value="new_ip" @update:options="getHistory" multi-sort density="compact">
+                    item-value="new_ip" @update:options="getHistory" multi-sort :sort-by="sortItems" density="compact">
                 </v-data-table-server>
             </v-card-text>
         </v-card>
@@ -28,12 +33,17 @@ const headers = [
     { title: "updated", key: "updated" },
 ];
 
+const sortItems = ref([]);
 
 const loading = ref(true);
 const total = ref(0);
 
 const histories = ref<History[]>([]);
 
+async function reset() {
+    sortItems.value = [];
+    await getHistory({ page: 1, itemsPerPage: 10, sortBy: [] });
+}
 
 async function getHistory({ page, itemsPerPage, sortBy }: { page: number, itemsPerPage: number, sortBy: SortItem[] }) {
     loading.value = true;
