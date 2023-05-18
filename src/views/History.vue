7e9b140c88915a9cdf24ce2b4ce6c0858f1daf5f
KotaHv <92137267+KotaHv@users.noreply.github.com>
2023-05-18 11:29:27 +0800

    feat: multiSort btn

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,6 +5,7 @@
                 <v-row justify="space-between">
                     <v-col cols="auto">IP History</v-col>
                     <v-col cols="auto">
+                        <v-btn @click="multi" variant="text" elevation="0" :icon="multiIcon"></v-btn>
                         <v-btn @click="reset" variant="text" elevation="0" icon="mdi-sort-variant-remove"></v-btn>
                     </v-col>
                 </v-row>
@@ -12,7 +13,8 @@
             <v-divider />
             <v-card-text class="table-container">
                 <v-data-table-server :headers="headers" :items="histories" :loading="loading" :items-length="total"
-                    item-value="new_ip" @update:options="getHistory" multi-sort :sort-by="sortItems" density="compact">
+                    item-value="new_ip" @update:options="getHistory" :multi-sort="multiSort" :sort-by="sortItems"
+                    density="compact">
                 </v-data-table-server>
             </v-card-text>
         </v-card>
@@ -40,9 +42,21 @@ const total = ref(0);
 
 const histories = ref<History[]>([]);
 
-async function reset() {
+const multiSort = ref(false);
+
+const multiIcon = ref("mdi-select-multiple");
+
+
+const multiIcons = ["mdi-select-multiple", "mdi-select-multiple-marker"]
+
+function multi() {
+    multiSort.value = !multiSort.value;
+    multiIcon.value = multiIcons[multiSort.value ? 1 : 0];
+    reset();
+}
+
+function reset() {
     sortItems.value = [];
-    await getHistory({ page: 1, itemsPerPage: 10, sortBy: [] });
 }
 
 async function getHistory({ page, itemsPerPage, sortBy }: { page: number, itemsPerPage: number, sortBy: SortItem[] }) {
